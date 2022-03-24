9ce53e570a0076741ec705ca765d9aff7bf4fab3
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-03-24 15:29:44 +0900

    clone my code

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -33,6 +33,7 @@ const Shop = () => {
             <div className="cart-container">
                 <h4>Order Summary</h4>
                 <p>Selected Items: {cart.length}</p>
+                <p>This is my product</p>
             </div>
         </div>
     );
